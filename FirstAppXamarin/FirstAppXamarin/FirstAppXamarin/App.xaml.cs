7099ff8af3a845f4f0a53d1177e3930522a5a3f6
Carlos-AO <93808290+Carlos-AO@users.noreply.github.com>
2024-02-19 13:51:20 +1000

    Added 2 basic apps

---
+++ FirstAppXamarin/FirstAppXamarin/FirstAppXamarin/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using FirstAppXamarin.Services;
+using FirstAppXamarin.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstAppXamarin
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
